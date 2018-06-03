b2cf947da97580b1ddbe65d802896a5a2b42a093
Kamil Wójcik <kamil.wojcik@cognifide.com>
2018-06-03 12:41:47 +0200

    Salesman Problem

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-"""
-This Python code is based on Java code by Lee Jacobson found in an article
-entitled "Applying a genetic algorithm to the travelling salesman problem"
-that can be found at: http://goo.gl/cJEY1
-"""
-
 import math
 import random
 from city import City
